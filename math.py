fa23bd33e72b1f71495d6b93cacd472b3e292882
RafaelRara <rafael_rar@hotmail.com>
2021-11-05 20:41:45 -0300

    add new lessons

---
+++ math.py
@@ -0,0 +1,28 @@
+#Execute para ver as funcionalidades
+# Comandos com números e mátematica
+
+# Arredondar com 2 casas 
+pi = round (3.1415,2)
+print (pi)
+
+# Comparações
+print (2 == 2)
+print (2 !=2 )
+
+# Conversões
+a = int(pi)
+print (a)
+
+b = float(a)
+print (b)
+
+# Números em lista
+lista = [1,2,3,4,5]
+
+maior = max(lista)
+print (maior)
+
+menor = min(lista)
+print (menor)
+
+print (len(lista))
---
+++ print_examples.py
@@ -0,0 +1,21 @@
+# Execute para ver as diferenças
+
+# Exemplos para serem usados com print:
+print ("Hello World")
+print ('Hello,"World"')
+print ('I\'m RAR') 
+print (2+2)
+print (6//3,end=" ")                # end=""  define como vai ser o final - nesse caso vazio
+print (6/3,"\n")                    # \n      É usado para quebra de linha
+print (3, "oi", 5,True, sep="-")    # sep="-" É usado para separar argumentos 
+print (3* "Oi")                     # Multiplicador de string
+
+# Declaração de variáveis
+x = 5          
+y = "3.9"
+a,b = 0
+m,n,o = 2,4,6
+
+print ("Python version: " + y) # Concatenar com String
+print ("O valor é:",x)         # Concatenar com int/float/double...
+
